725cc8a85bcc6a3afd9ca3c60d046c4cd851593b
Fervolpato <fervolpato1991@gmail.com>
2023-07-14 01:36:18 -0300

    cambios4

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 
 conn.sync().then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('Listening at', process.env.PORT);
+  server.listen('3001', () => {
+    console.log('Listening at 3001');
   });
 });
