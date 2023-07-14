a257e0d4250c4994c8383fb36418eff4cfe57130
Fervolpato <fervolpato1991@gmail.com>
2023-07-14 00:54:08 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log('Listening at', PORT); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('Listening at', process.env.PORT);
   });
 });
